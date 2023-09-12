ce73b350505911675565a06263b8c11ac978ec6b
eszkudzi <ewa.szkudzinska@gmail.com>
2023-09-12 21:09:10 +0200

    Generic Stack

--- MotoApp/MotoApp/Program.cs
+++ MotoApp/MotoApp/Program.cs
@@ -1,14 +1,9 @@
-﻿using MotoApp;
-var stack = new BasicStack();
-stack.Push(4.5);
-stack.Push(43);
-stack.Push(333.6);
+﻿using MotoApp.Entities;
+using MotoApp.Repositories;
 
-double sum = 0.0;
+var employeeRepository = new GenericRepository<Employee>();
+employeeRepository.Add(new Employee { FirstName = "Ewa"});
+employeeRepository.Add(new Employee { FirstName = "Piotr" });
+employeeRepository.Add(new Employee { FirstName = "Adam" });
 
-while (stack.Count >0)
-{
-    double item = stack.Pop();
-    Console.WriteLine($"Item: {item}");
-    sum += item;
-}
+employeeRepository.Save();
