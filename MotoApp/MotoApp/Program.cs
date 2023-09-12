f5a10c3d7e060b95921c227d1fac506bfc536f20
eszkudzi <ewa.szkudzinska@gmail.com>
2023-09-12 21:00:23 +0200

    BasickStack

---
+++ MotoApp/MotoApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using MotoApp;
+var stack = new BasicStack();
+stack.Push(4.5);
+stack.Push(43);
+stack.Push(333.6);
+
+double sum = 0.0;
+
+while (stack.Count >0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+    sum += item;
+}
