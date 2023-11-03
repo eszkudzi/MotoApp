9d6e0db8bae496f93d84cf2506bc05ff35156807
eszkudzi <ewa.szkudzinska@gmail.com>
2023-11-03 13:53:01 +0100

    Changes in structure

---
+++ MotoApp/MotoApp/Data/Entities/Car.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+namespace MotoApp.Entities
+{
+    public class Car: EntityBase
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public decimal StandardCost { get; set; }
+        public decimal ListPrice { get; set; }
+        public string Type { get; set; }
+
+        //Calculated Properties
+        public int? NameLength { get; set; }
+        public decimal? TotalSales { get; set; }
+
+    }
+}
