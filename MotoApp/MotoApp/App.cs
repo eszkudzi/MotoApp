d24152f2d9d4740fea15142e52d3fd6e966de835
eszkudzi <ewa.szkudzinska@gmail.com>
2023-11-19 15:28:31 +0100

    Methods for database in App.cs

--- MotoApp/MotoApp/App.cs
+++ MotoApp/MotoApp/App.cs
@@ -1,4 +1,5 @@
 ﻿using MotoApp.Components.CsvReader;
+using MotoApp.Components.CsvReader.Models;
 using MotoApp.Data;
 
 namespace MotoApp
@@ -14,8 +15,59 @@ namespace MotoApp
             _motoAppDbContext = motoAppDbContext;
             _motoAppDbContext.Database.EnsureCreated();
         }
-
         public void Run()
+        {
+            // InsertData();
+            // ReadAllCarsFromDb();
+            // ReadGroupedlCarsFromDb();
+
+            var cayman = this.ReadFirst("Cayman");
+            //cayman.Name = "Mój Samochód";
+            //_motoAppDbContext.SaveChanges();
+
+            _motoAppDbContext.Cars.Remove(cayman);
+            _motoAppDbContext.SaveChanges();
+        }
+
+        public Car? ReadFirst (string name)
+        {
+            return _motoAppDbContext.Cars.FirstOrDefault(x => x.Name == name);
+        }
+
+        public void ReadGroupedlCarsFromDb()
+        {
+            var groups = _motoAppDbContext
+                .Cars
+                .GroupBy(x => x.Manufacturer)
+                .Select(x => new
+                {
+                    Name = x.Key,
+                    Cars = x.ToList()
+                })
+                .ToList();
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine(group.Name);
+                Console.WriteLine("========");
+                foreach (var car in group.Cars)
+                {
+                    Console.WriteLine($"\t{car.Name}: {car.Combined}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void ReadAllCarsFromDb()
+        {
+            var carsFromDb = _motoAppDbContext.Cars.ToList();
+            foreach (var carFromDB in carsFromDb)
+            {
+                Console.WriteLine($"\t{carFromDB.Name}: {carFromDB.Combined}");
+            }
+        }
+
+        public void InsertData()
         {
             var cars = _csvReader.ProcessCars("Resources\\Files\\fuel.csv");
 
